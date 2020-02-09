bd6bd6d7bebf17084f1e83fe81c4bb4e0cdeab84
Vlad <59834032+Kikkarado@users.noreply.github.com>
2020-02-09 16:39:55 +0200

    Updated List

--- app/src/main/java/com/example/resistorcalculation/DBActivity.kt
+++ app/src/main/java/com/example/resistorcalculation/DBActivity.kt
@@ -1,10 +1,15 @@
 package com.example.resistorcalculation
 
+import android.content.Intent
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.SimpleCursorAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_db.*
 
 class DBActivity : AppCompatActivity() {
@@ -14,18 +19,44 @@ class DBActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_db)
+        setSupportActionBar(toolbar1)
 
         database = DBHelper(this).readableDatabase
-        cursor = database?.rawQuery("select * from $TABLE_DATA", null)
-
+        cursor = database?.rawQuery("select * from $TABLE_DATA order by $KEY_ID desc", null)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.back ->{
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+            R.id.delete ->{
+                database?.delete(TABLE_DATA, null, null)
+                finish()
+                overridePendingTransition(0, 0)
+                startActivity(intent)
+                overridePendingTransition(0, 0)
+            }
+        }
+
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.back_delete, menu)
+        return true
+    }
     override fun onResume() {
         super.onResume()
-        val dataDB = arrayOf(RESISTENCE, MIN_POWER)
-        val dataAdapter = SimpleCursorAdapter(this, android.R.layout.two_line_list_item,
-            cursor, dataDB, intArrayOf(android.R.id.text1, android.R.id.text2), 0)
-        listview.adapter = dataAdapter
+        val dataDB = arrayOf(KEY_ID ,VOLTAGE, F_VOLTAGE, CURRENT, N_O_LEDS, RESISTENCE, MIN_POWER)
+        if(cursor != null && cursor?.count!! > 0){
+        val dataAdapter = SimpleCursorAdapter(this, R.layout.list,
+            cursor, dataDB, intArrayOf(R.id.number, R.id.text1, R.id.text2, R.id.text3, R.id.text4, R.id.text5, R.id.text6), 0)
+        listview.adapter = dataAdapter}
+        else {
+           val toast = Toast.makeText(this, "Вы не проводили расчётов.", Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()}
     }
 
     override fun onDestroy() {
